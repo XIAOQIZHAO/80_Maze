d8a59dfe92c64f30e232099fedd5a9b105dd1608
Your Name <xzhao00@stuy.edu>
2019-06-14 13:21:18 -0400

    v0-1

--- Maze.java
+++ Maze.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
+
 public class Maze {
     private boolean[][] maze;
-
+    private static final String INDENT = "         ";
     public Maze( int row, int column){
 	maze = new boolean[row][column];
     }
@@ -17,10 +19,41 @@ public class Maze {
     }
 
     public String toString() {
-	int row = maze.row();
-	int column = maze.column();
-	String pic = INDENT + row + "*" + column + " maze"
-	    + "with walls"
-	    + wallCordinates;
+	int row = row();
+	int column = column();
+	ArrayList<String> wallCoordinates = new ArrayList<String>();
+	for (int i = 0; i < row; i++)
+	    for (int j = 0; j < column; j++)
+		if (maze[i][j] == false)
+		    wallCoordinates.add("[" + i + "," + j + "] ");
+	String pic = System.lineSeparator()
+	    + INDENT + row + "*" + column + " maze "
+    	    + "with walls: "
+	    + System.lineSeparator()
+	    + wallCoordinates;
 	
+	String underscores = "";
+	
+	if (row == 0 & column == 0) return pic;
+	
+	pic += INDENT + "columns" + System.lineSeparator() +INDENT;
+	
+	for (int c = 0; c < column; c++){
+	    pic += String.format("%-3d", c);
+	    underscores += "ooo";
+	}
+
+	pic += INDENT + underscores + System.lineSeparator();
+	
+	for(int r = 0; r < row; r++){
+            pic += String.format("r %2d|", r);
+            for( int c  = 0; c < column; c++)
+		if (maze[r][c])
+		    pic += "_";
+		else
+	            pic += " X ";
+            pic += System.lineSeparator();
+	}
+	return pic;
+	}
 }
--- UserOfMaze.java
+++ UserOfMaze.java
@@ -1,11 +1,11 @@
 public class UserOfMaze {
     public static void main (String[] commandLine) {
-	Maze maze = new Maze( 0,0);
+	Maze maze = new Maze( 1,0);
 	oneTest(maze);
     }
 
     private static void oneTest (Maze maze){
-	System.out.println(maze.row() + "*" + maze.column() + " maze");
+	System.out.println(maze);
     }
 	
 }
